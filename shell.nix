3037b4abd50a8bca01fa77ac3eb2c98346c69fcb
Petr Laštovička <lastope2@fit.cvut.cz>
2024-11-16 17:11:59 +0200

    chore: Added Nix shell file describing the required environment

---
+++ shell.nix
@@ -0,0 +1,52 @@
+# TODO convert to flake
+{
+  pkgs ? import <nixpkgs> { },
+}:
+pkgs.mkShell {
+  name = "PyRigi shell";
+
+  packages = [
+    (pkgs.python312.withPackages (
+      python-pkgs: with python-pkgs; [
+        pandas
+        numpy
+        matplotlib
+        scipy
+        sympy
+        networkx
+        pytest
+        pytest-benchmark
+        pytest-timeout
+        tqdm
+        urllib3
+
+        # notebook
+        # ipython
+        # jupyter
+      ]
+    ))
+    (pkgs.python311.withPackages (
+      python-pkgs: with python-pkgs; [
+        # these do not support python 3.12 yet
+        notebook
+        ipython
+        jupyter
+
+        pandas
+        numpy
+        matplotlib
+        scipy
+        sympy
+        networkx
+        pytest
+        pytest-benchmark
+        pytest-timeout
+        tqdm
+        urllib3
+      ]
+    ))
+    pkgs.jupyter-all
+  ];
+
+  shellHook = '''';
+}
