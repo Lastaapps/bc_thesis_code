1d50baa9a8d140ace5b6047c0401ead19f5a209c
Petr Laštovička <lastope2@fit.cvut.cz>
2025-03-16 22:22:47 +0200

    build: PyRigi 1.0.0

--- shell.nix
+++ shell.nix
@@ -20,6 +20,7 @@ pkgs.mkShell {
     # https://nixos.wiki/wiki/Packaging/Quirks_and_Caveats
     # fixes libstdc++ issues and libgl.so issues
     LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib/:$LD_LIBRARY_PATH
+    LD_LIBRARY_PATH=${pkgs.graphviz}/lib/:$LD_LIBRARY_PATH
 
     if [ ! -d .venv ]; then
       python3 -m venv .venv
@@ -27,33 +28,6 @@ pkgs.mkShell {
     source ./.venv/bin/activate
 
 
-    pip install -r ./requirements.txt
-    # pip install \
-    # \
-    # pandas~=2.2.0 \
-    # numpy~=2.1.0 \
-    # scipy~=1.14.0 \
-    # sympy~=1.13.0 \
-    # matplotlib~=3.9.0 \
-    # \
-    # networkx~=3.4.0 \
-    # pygraphviz==1.14 \
-    # \
-    # pytest~=8.3.0 \
-    # pytest-benchmark~=5.1.0 \
-    # pytest-timeout~=2.3.0 \
-    # \
-    # tqdm~=4.67.0 \
-    # urllib3~=2.2.0 \
-    # distinctipy~=1.3.0 \
-    # \
-    # ipywidgets~=8.1.0 \
-    # ipycanvas~=0.13.0 \
-    # ipyevents~=2.0.0 \
-    # \
-    # notebook~=7.2.0 \
-    # ipython~=8.29.0 \
-    # ipykernel~=6.29.0 \
-    # jupyterlab~=4.2.0 \
+    pip install -r ${./requirements.txt}
   '';
 }
